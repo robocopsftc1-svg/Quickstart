bbbd702857e5fe0df1c2417e3e0191ffd5ea3504
faithe124ere <zouethan06@gmail.com>
2026-09-12 17:48:01 -0500

    for tomorrow

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ShootingLogic.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class ShootingLogic {
+    // ---------------- init first motors ---------------
+
+    //example inits
+    //private Servo gateServo; //note the use of private to ensure random other classes don't interfere and corrupt anything
+    //private DcMotor spinner;
+
+    private ElapsedTime stateTimer = new ElapsedTime();
+
+
+    private enum ShootingState {
+        //we'll write the flowchart and add which state's we'll want to use for our shooting mechanism
+    }
+
+
+
+
+
+
+
+
+
+}
