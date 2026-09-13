342ede0cb9ae479f3d9b7e841a353292a6cf7e3f
faithe124ere <zouethan06@gmail.com>
2026-09-13 12:32:17 -0500

    constants for pedropathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,11 +1,38 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(10);
+
+    public static MecanumConfig driveConfig = new MecanumConfig(
+            c -> {
+                c.frontLeftName.set("left_front");
+                c.backLeftName.set("left_back");
+                c.frontRightName.set("right_front");
+                c.backRightName.set("right_back");
+
+                c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+                c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+                c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+                c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+            }
+    );
+
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 67);
+
+
     public static Follower create(HardwareMap h) {
-        // return new Follower(Drivetrain, Localizer, Foresight);
+        .pathConstraints(pathConstraints)
+        .mecanumDrivetrain(driveConfig)
+        .build();
         return null;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.tuning.autotune.Procedure;
+import com.pedropathing.tuning.autotune.Tuner;
+
+import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.TwoWheelTuner;
+
 public class Tuning {
     // Tuners go here
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
+
 }
