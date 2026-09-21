9ced6d8b8e784abbbfd5696e354f3a863faf6727
PDS Code <li.zou8@outlook.com>
2026-09-20 20:54:46 -0500

    must tune localizer then we can tune the foresight algorithm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroTuner.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.revhub.localizers.PinpointLocalizer;
 import com.pedropathing.tuning.autotune.Procedure;
 import com.pedropathing.tuning.autotune.Tuner;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.pedro.Constants;
+import org.firstinspires.ftc.teamcode.pedro.procedures.ForesightTuner;
 import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
 import org.firstinspires.ftc.teamcode.pedro.procedures.PinpointTuner;
 
@@ -24,6 +28,10 @@ public class pedroTuner extends OpMode {
         return new PinpointTuner();
     }
 
+    @Tuner
+    public static Procedure foresightTuner() {
+        return new ForesightTuner((hardwareMap) -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig), (hardwareMap) -> new Mecanum(hardwareMap, Constants.drivetrainConfig));
+    }
     @Override
     public void init(){
         //
